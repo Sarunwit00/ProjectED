6f10b768560c74d1171c05280c499069a2b6d57e
Sarunwit00 <a.sarunwit@gmail.com>
2025-11-04 12:19:59 +0700

    Save2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <Navbar />
-      <Home />
     </div>
   );
 }
