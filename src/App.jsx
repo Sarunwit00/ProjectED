fdb1310ff9565b3f477421c8e616532e5429f2c9
Sarunwit00 <a.sarunwit@gmail.com>
2025-11-04 12:09:27 +0700

    Save not

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white text-gray-900">
       <Navbar />
-      <main className="mx-auto flex max-w-6xl flex-col items-center justify-center px-4 py-24 sm:py-32">
-        <h1 className="text-2xl font-medium sm:text-3xl">Translate dialect</h1>
-      </main>
+      <Home />
     </div>
   );
 }
